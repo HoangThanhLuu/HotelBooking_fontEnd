1714a55a33f4f8771eb86200b05273cc48618634
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-27 16:39:12 +0700

    project hotel

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import AddRoom from "./components/room/AddRoom";
+
+const App = () => {
+  return (
+    <div>
+      <AddRoom />
+    </div>
+  );
+};
+
+export default App;
