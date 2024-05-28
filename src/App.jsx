f4d9cd70249a771f437bfa68f150f13e9d660c32
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-29 02:22:56 +0700

    Router NavBar Footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,24 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddRoom from "./components/room/AddRoom";
 import Home from "./components/home/Home";
 import EditRoom from "./components/room/EditRoom";
+import Footer from "./components/layout/Footer";
+import NavBar from "./components/layout/NavBar";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/edit-room/:roomId" element={<EditRoom />} />
-          <Route path="/existing-rooms" element={<ExistingRooms />} />
-          <Route path="/add-room" element={<AddRoom />} />
-        </Routes>
-      </Router>
+      <main>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/edit-room/:roomId" element={<EditRoom />} />
+            <Route path="/existing-rooms" element={<ExistingRooms />} />
+            <Route path="/add-room" element={<AddRoom />} />
+          </Routes>
+        </Router>
+        <Footer />
+      </main>
     </>
   );
 }
