59d2b782afdaaaaeaddc47cc45fabcaa6182f7b1
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-29 01:04:16 +0700

    Link addRoom là existing-rooms

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import AddRoom from "./components/room/AddRoom";
+import ExistingRooms from "./components/room/ExistingRooms";
 
 const App = () => {
   return (
     <div>
       <AddRoom />
+      <ExistingRooms />
     </div>
   );
 };
