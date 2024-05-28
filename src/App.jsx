a8c0462d46cc1964ed88c3f9b4ca5a74377b6962
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-29 01:49:11 +0700

    Router EditRoom

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ExistingRooms from "./components/room/ExistingRooms";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddRoom from "./components/room/AddRoom";
 import Home from "./components/home/Home";
+import EditRoom from "./components/room/EditRoom";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/edit-room/:roomId" element={<EditRoom />} />
           <Route path="/existing-rooms" element={<ExistingRooms />} />
           <Route path="/add-room" element={<AddRoom />} />
         </Routes>
