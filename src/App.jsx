3d97fc370c17509840d92f8665c770acad0829b6
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-29 01:47:41 +0700

    Router Home

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import "/node_modules/bootstrap/dist/js/bootstrap.min.js";
 import ExistingRooms from "./components/room/ExistingRooms";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddRoom from "./components/room/AddRoom";
+import Home from "./components/home/Home";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/existing-rooms" element={<ExistingRooms />} />
-        <Route path="/add-room" element={<AddRoom />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/existing-rooms" element={<ExistingRooms />} />
+          <Route path="/add-room" element={<AddRoom />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
