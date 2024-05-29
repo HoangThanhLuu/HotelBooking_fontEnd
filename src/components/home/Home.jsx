0eaa89dafaf60e8aff7f732d48e23180b5ba5cc8
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-29 14:48:15 +0700

    RoomCarousel bên Home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,15 +3,19 @@ import MainHeader from "../layout/MainHeader";
 import HotelService from "../common/HotelService";
 import Parallax from "../common/Parallax";
 import { useLocation } from "react-router-dom";
+import RoomCarousel from "../common/RoomCarousel";
 
 const Home = () => {
   return (
     <section>
       <MainHeader />
       <div className="container">
+        <RoomCarousel />
         <Parallax />
+        <RoomCarousel />
         <HotelService />
         <Parallax />
+        <RoomCarousel />
       </div>
     </section>
   );
