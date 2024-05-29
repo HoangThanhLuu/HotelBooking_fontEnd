4ed8c3f1279e091d7c7d2b860c7de3361f51e4b9
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-29 14:46:54 +0700

    RoomCarousel ,useEffect

--- src/components/common/RoomCarousel.jsx
+++ src/components/common/RoomCarousel.jsx
@@ -10,7 +10,25 @@ const RoomCarousel = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    setIsLoading(true);
+    getAllRooms()
+      .then((data) => {
+        setRooms(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <div className="mt-5">Loading rooms....</div>;
+  }
+  if (errorMessage) {
+    return <div className=" text-danger mb-5 mt-5">Error : {errorMessage}</div>;
+  }
 
   return (
     <section className="bg-light mb-5 mt-5 shadow">
