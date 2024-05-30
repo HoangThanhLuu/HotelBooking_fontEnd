0bc22be2a856b974b8234fc96fa8fec99ceff3e6
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-30 21:05:43 +0700

    BookingSummary,useEffect handleConfirmBooking

---
+++ src/components/booking/BookingSummary.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
+
+const BookingSummary = ({ booking, payment, isFormValid, onConfirm }) => {
+  const checkInDate = moment(booking.checkInDate);
+  const checkOutDate = moment(booking.checkOutDate);
+  const numberOfDays = checkOutDate.diff(checkInDate, "days");
+  const [isBookingConfirmed, setIsBookingConfirmed] = useState(false);
+  const [isProcessingPayment, setIsProcessingPayment] = useState(false);
+  const navigate = useNavigate();
+
+  const handleConfirmBooking = () => {
+    setIsProcessingPayment(true);
+    setTimeout(() => {
+      setIsProcessingPayment(false);
+      setIsBookingConfirmed(true);
+      onConfirm();
+    }, 3000);
+  };
+
+  useEffect(() => {
+    if (isBookingConfirmed) {
+      navigate("/booking-success");
+    }
+  }, [isBookingConfirmed, navigate]);
+
+  return <div className="row"></div>;
+};
+
+export default BookingSummary;
