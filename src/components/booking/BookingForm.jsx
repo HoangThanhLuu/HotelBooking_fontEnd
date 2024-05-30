46321a13633666542dd96a66a677188f3b967c1e
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-30 21:00:29 +0700

    BookingForm,UseEffect và handle

--- src/components/booking/BookingForm.jsx
+++ src/components/booking/BookingForm.jsx
@@ -27,23 +27,78 @@ const BookingForm = () => {
   const { roomId } = useParams();
   const navigate = useNavigate();
 
-  const handleInputChange = (e) => {};
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setBooking({ ...booking, [name]: value });
+    setErrorMessage("");
+  };
 
-  const getRoomPriceById = async (roomId) => {};
+  const getRoomPriceById = async (roomId) => {
+    try {
+      const response = await getRoomById(roomId);
+      setRoomPrice(response.roomPrice);
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
 
   useEffect(() => {
     getRoomPriceById(roomId);
   }, [roomId]);
 
-  const calculatePayment = () => {};
+  const calculatePayment = () => {
+    const checkInDate = moment(booking.checkInDate);
+    const checkOutDate = moment(booking.checkOutDate);
+    const diffInDays = checkOutDate.diff(checkInDate, "days");
+    const paymentPerDay = roomPrice ? roomPrice : 0;
+    return diffInDays * paymentPerDay;
+  };
 
-  const isGuestCountValid = () => {};
+  const isGuestCountValid = () => {
+    const adultCount = parseInt(booking.numOfAdults);
+    const childrenCount = parseInt(booking.numOfChildren);
+    const totalCount = adultCount + childrenCount;
+    return totalCount >= 1 && adultCount >= 1;
+  };
 
-  const isCheckOutDateValid = () => {};
+  const isCheckOutDateValid = () => {
+    if (
+      !moment(booking.checkOutDate).isSameOrAfter(moment(booking.checkInDate))
+    ) {
+      setErrorMessage("Check-out date must be after check-in date");
+      return false;
+    } else {
+      setErrorMessage("");
+      return true;
+    }
+  };
 
-  const handleSubmit = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    if (
+      form.checkValidity() === false ||
+      !isGuestCountValid() ||
+      !isCheckOutDateValid()
+    ) {
+      e.stopPropagation();
+    } else {
+      setIsSubmitted(true);
+    }
+    setValidated(true);
+  };
 
-  const handleFormSubmit = async () => {};
+  const handleFormSubmit = async () => {
+    try {
+      const confirmationCode = await bookRoom(roomId, booking);
+      setIsSubmitted(true);
+      navigate("/booking-success", { state: { message: confirmationCode } });
+    } catch (error) {
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      navigate("/booking-success", { state: { error: errorMessage } });
+    }
+  };
 
   return (
     <>
